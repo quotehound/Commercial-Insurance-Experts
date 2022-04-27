0daaa41bfcc70d77165ca717b9126ae2c48f952d
David Verduzco <davidverduzco@Davids-MacBook-Pro-2.local>
2022-04-27 13:41:48 -0700

    MA fix

--- src/MA.jsx
+++ src/MA.jsx
@@ -8,7 +8,7 @@ class MA extends Component {
 
         const renderScript = document.createElement("script");
 
-        //  renderScript.innerHTML = "window.MediaAlphaExchange = {'data': { 'zip': document.getElementById('zipCode').value}, 'placement_id': 'YiPFAJc_r0i9fsZr0uP7vvicsinK3Q','sub_1': 'test sub id', 'type': 'ad_unit', 'version': 17}; window.MediaAlphaExchange__load('mediaalpha_placeholder'); ";
+         renderScript.innerHTML = "window.MediaAlphaExchange = {'data': { 'zip': document.getElementById('zipCode').value}, 'placement_id': '4yclnD1Pz-JicFFmiW0DhV7a86VXHw','sub_1': 'test sub id', 'type': 'ad_unit', 'version': 17}; window.MediaAlphaExchange__load('mediaalpha_placeholder'); ";
 
          document.body.appendChild(renderScript);
 
@@ -25,7 +25,9 @@ class MA extends Component {
 
 
 
-                <div/>
+            <div id="mediaalpha_placeholder">
+            </div>
+            
 
 
    
